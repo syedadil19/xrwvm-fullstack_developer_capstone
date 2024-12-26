7dbf5af6f41561cda9fc5b3272fa8a0f6432ba7a
Syed Mohamad Adil Bin Syed Mohd Ejaz Anver <148873282+syedadil19@users.noreply.github.com>
2024-12-26 16:28:03 +0800

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,3 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -6,31 +5,23 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # path for registration
-    path(route='register', view=views.registration_request, name='registeration'),
-
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
-
-    # path for logout
-    path(route='logout', view=views.logout_request, name='logout'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-    path(route='add_review', view=views.add_review, name='add_review'),
-
-    # path for get_cars
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-
-    # path for get_dealerships
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-
-    # path for. get_dealer_details
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-
-    # path for get_dealer_reviews
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path(route='register', view=views.registration_request,
+         name='registeration'),
+    path(route='login', view=views.login_user,
+         name='login'),
+    path(route='logout', view=views.logout_request,
+         name='logout'),
+    path(route='add_review', view=views.add_review,
+         name='add_review'),
+    path(route='get_cars', view=views.get_cars,
+         name='get_cars'),
+    path(route='get_dealers', view=views.get_dealerships,
+         name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships,
+         name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details,
+         name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews,
+         name='dealer_reviews'),
+] + static(settings.MEDIA_URL,
+           document_root=settings.MEDIA_ROOT)
