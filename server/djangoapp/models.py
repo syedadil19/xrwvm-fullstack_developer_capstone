d24375197ae32a27c5011497aa483e5fc3e4df12
Syed Mohamad Adil Bin Syed Mohd Ejaz Anver <148873282+syedadil19@users.noreply.github.com>
2024-12-26 16:09:29 +0800

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,30 +3,25 @@ from django.db import models
 from django.contrib import admin
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
 # Car Make Model
 class CarMake(models.Model):
     name = models.CharField(
         max_length=100, 
         help_text="Enter the car make (e.g., Toyota, Ford)"
     )
-    description = models.TextField
-    (
+    description = models.TextField(
         help_text="Enter the description of the car make"
     )
 
     def __str__(self):
         return self.name
 
-
 # Car Model
 class CarModel(models.Model):
-    car_make = models.ForeignKey
-    (
+    car_make = models.ForeignKey(
         CarMake, on_delete=models.CASCADE, related_name='car_models'
     )
-    name = models.CharField
-    (
+    name = models.CharField(
         max_length=100, 
         help_text="Enter the car model (e.g., Camry, Mustang)"
     )
