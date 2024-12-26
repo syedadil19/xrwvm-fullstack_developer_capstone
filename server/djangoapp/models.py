b707b5e9a2b8b033d3a470d189c06cb02701cb20
Syed Mohamad Adil Bin Syed Mohd Ejaz Anver <148873282+syedadil19@users.noreply.github.com>
2024-12-26 15:53:07 +0800

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,29 +3,35 @@ from django.db import models
 from django.contrib import admin
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
 # Car Make Model
 class CarMake(models.Model):
-    name = models.CharField(max_length=100, help_text="Enter the car make (e.g., Toyota, Ford)")
-    description = models.TextField(help_text="Enter the description of the car make")
+    name = models.CharField(
+        max_length=100, 
+        help_text="Enter the car make (e.g., Toyota, Ford)"
+    )
+    description = models.TextField(
+        help_text="Enter the description of the car make"
+    )
 
     def __str__(self):
         return self.name
-
 
 # Car Model
 class CarModel(models.Model):
     car_make = models.ForeignKey(
         CarMake, on_delete=models.CASCADE, related_name='car_models'
     )
-    name = models.CharField(max_length=100, help_text="Enter the car model (e.g., Camry, Mustang)")
+    name = models.CharField(
+        max_length=100, 
+        help_text="Enter the car model (e.g., Camry, Mustang)"
+    )
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
     ]
     type = models.CharField(
-        max_length=10, choices=CAR_TYPES, default='SEDAN', 
+        max_length=10, choices=CAR_TYPES, default='SEDAN',
         help_text="Select the car type"
     )
     year = models.IntegerField(
@@ -36,7 +42,6 @@ class CarModel(models.Model):
 
     def __str__(self):
         return f"{self.car_make.name} {self.name}"
-
 
 # Register models with admin
 admin.site.register(CarMake)
