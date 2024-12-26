73f624f912adb649c189660c0080840b43e6d995
Syed Mohamad Adil Bin Syed Mohd Ejaz Anver <148873282+syedadil19@users.noreply.github.com>
2024-12-26 16:25:32 +0800

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -8,7 +8,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Car Make Model
 class CarMake(models.Model):
     name = models.CharField(
-        max_length=100, 
+        max_length=100,
         help_text="Enter the car make (e.g., Toyota, Ford)"
     )
     description = models.TextField(
@@ -25,7 +25,7 @@ class CarModel(models.Model):
         CarMake, on_delete=models.CASCADE, related_name='car_models'
     )
     name = models.CharField(
-        max_length=100, 
+        max_length=100,
         help_text="Enter the car model (e.g., Camry, Mustang)"
     )
     CAR_TYPES = [
@@ -38,7 +38,7 @@ class CarModel(models.Model):
         help_text="Select the car type"
     )
     year = models.IntegerField(
-        default=2023, 
+        default=2023,
         validators=[MaxValueValidator(2023), MinValueValidator(2015)],
         help_text="Enter the model year between 2015 and 2023"
     )
